da8a901b81c465b3bae7d76bd248b2267b9cdec1
SpacyFranky <bigaziz@outlook.com>
2020-02-24 21:28:46 +0000

    Textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
-print (ARGV[0].scan(/(?<=from:)[a-zA-Z]*/).join)
+print (ARGV[0].scan(/(?<=from:)[a-zA-Z+0-9]*/).join)
 print ","
-print (ARGV[0].scan(/(?<=to:)[+][0-9]*/).join)
+print (ARGV[0].scan(/(?<=to:)[+0-9]*/).join)
 print ","
-print (ARGV[0].scan(/(?<=flags:)(...)(...)(...)(...)/).join)
+print (ARGV[0].scan(/(?<=flags:)(?<=flags:).[0-1:-]*/).join)
 puts
