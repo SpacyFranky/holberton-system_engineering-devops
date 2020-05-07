a16d05879a800b20e783a383ff2f9180cd820b8e
spacyfranky <bigaziz@outlook.com>
2020-05-07 19:18:09 +0000

    returns a list containing the titles of all hot articles for a given subreddit

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,32 +8,7 @@
 import requests
 
 
-# Iterative Method
-
-#def recurse(subreddit):
-    #"""returns a list containing the titles of all hot articles
-    #   for a given subreddit.
-    #   if no results are found, it returns None.
-    #"""
-
-    #url = 'https://www.reddit.com/r/{}/hot.json?limit=100'.format(subreddit)
-    #r = requests.get(url, headers={'User-agent': 'hello-student 0.4'})
-    #hot = r.json()
-    #l = []
-    #v = 0
-    #while (v == 0):
-    #    l.extend(hot.get('data').get('children'))
-    #    if hot.get('data').get('after') is None:
-    #        v = 1
-    #    f = '?limit=100&after={}'.format(hot.get('data').get('after'))
-    #    url = 'https://www.reddit.com/r/{}/hot.json{}'.format(subreddit, f)
-    #    r = requests.get(url, headers={'User-agent': 'hello-student 0.4'})
-    #    hot = r.json()
-    #return l
-
-# Recursive Method
-
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], page=""):
     """main recursive function that handles initial page
     """
     try:
