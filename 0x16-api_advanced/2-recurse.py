4f7c4b3ab05ec8dc89722c04a504e5700c57cb62
spacyfranky <bigaziz@outlook.com>
2020-05-07 12:52:20 +0000

    recursing a get api method

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""This file contains a recursive function that queries the Reddit API
+   and returns a list containing the titles of all hot articles for
+   a given subreddit. If no results are found for the given subreddit,
+   the functio returns None.
+"""
+
+import requests
+
+
+def recurse(subreddit):
+    """returns a list containing the titles of all hot articles
+       for a given subreddit.
+       if no results are found, it returns None.
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=100'.format(subreddit)
+    r = requests.get(url, headers={'User-agent': 'hello-student 0.4'})
+    hot = r.json()
+    l = []
+    v = 0
+    while (v == 0):
+        l.extend(hot.get('data').get('children'))
+        if hot.get('data').get('after') is None:
+            v = 1
+        f = '?limit=100&after={}'.format(hot.get('data').get('after'))
+        url = 'https://www.reddit.com/r/{}/hot.json{}'.format(subreddit, f)
+        r = requests.get(url, headers={'User-agent': 'hello-student 0.4'})
+        hot = r.json()
+    return l
